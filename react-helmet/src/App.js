9e27500a48e20067147912e55b1bd926601e2d22
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2025-12-20 20:02:21 +0330

    Initialize React app with react-helmet-async
    Set up a new React project using Create React App and add react-helmet-async for managing document head. Includes initial project structure, dependencies, and a sample component demonstrating usage.

---
+++ react-helmet/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ReactHelmetSample from "./components/ReactHelmetSample";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Learn React Helmet Async</header>
+      <main>
+        <ReactHelmetSample />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-helmet/src/components/ReactHelmetSample.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+const ReactHelmetSample = () => {
+  return (
+    <>
+    <Helmet>
+        <title>React Helmet Sample Component</title>
+    </Helmet>
+      <h2>React Helmet Sample</h2>
+      <p>this is react helmet sample component.</p>
+    </>
+  );
+};
+
+export default ReactHelmetSample;
