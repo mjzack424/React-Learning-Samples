660ac3514c23f4b621c90cb24c24fdeb2bd1a7fe
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2025-12-21 09:41:18 +0330

    Add mui-rtl-clean React app and update portfolio-mui
    Added a new Create React App project 'mui-rtl-clean' with initial files and dependencies. Updated dependencies and assets in 'portfolio-mui' and 'react-icon-component', including changes to package.json, package-lock.json, favicon, and App.js.

--- portfolio-mui/src/App.js
+++ portfolio-mui/src/App.js
@@ -1,16 +1,45 @@
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { Button } from "@mui/material";
-import "./App.css";
+
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
+// RTL cache
+const rtlCache = createCache({
+  key: "mui-rtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+// RTL theme
+const theme = createTheme({
+  direction: "rtl",
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header</h1>
-      </header>
-      <main>
-        <p>Main</p>
-        <Button variant="contained">کلیک کن</Button>
-      </main>
-    </div>
+    <HelmetProvider>
+      <CacheProvider value={rtlCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+
+          <Helmet>
+            <title>وب سایت شخصی محمد جواد ذاکریان</title>
+          </Helmet>
+
+          <div style={{ padding: 24 }}>
+            <h1>هدر</h1>
+            <p>محتوا</p>
+            <Button variant="contained">کلیک کن</Button>
+          </div>
+        </ThemeProvider>
+      </CacheProvider>
+    </HelmetProvider>
   );
 }
+
 export default App;
