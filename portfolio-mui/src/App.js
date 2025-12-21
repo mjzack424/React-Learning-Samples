ae453970137fe48cc3ab50638ecefc679a832214
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2025-12-21 09:01:38 +0330

    Initialize portfolio-mui and react-icon-component apps
    Add initial project structure for two React applications: portfolio-mui and react-icon-component. Includes configuration files, public assets, source code, and documentation generated by Create React App.

---
+++ portfolio-mui/src/App.js
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+      </header>
+      <main>
+        <p>Main</p>
+        <Button variant="contained">کلیک کن</Button>
+      </main>
+    </div>
+  );
+}
+export default App;
---
+++ react-icon-component/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import {BsEmojiSmile} from 'react-icons/bs'
+import { FcStackOfPhotos } from "react-icons/fc";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">React Icons Component</header>
+      <main>
+        <BsEmojiSmile />
+        <FcStackOfPhotos />
+        <p>React Icons Component Sample</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
