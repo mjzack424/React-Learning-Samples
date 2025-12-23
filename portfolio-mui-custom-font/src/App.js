5613f42e1da4db11de644ca9c93d12c47c245a70
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2025-12-23 09:58:36 +0330

    129-Initialize React app with custom fonts and MUI
    Set up a new React project using Create React App, add Material-UI (MUI) dependencies, and configure custom fonts (Roboto and Vazir) with supporting CSS and assets. Includes initial project structure, configuration files, and documentation.

---
+++ portfolio-mui-custom-font/src/App.js
@@ -0,0 +1,50 @@
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+
+// قبل (قدیمی):
+//import rtlPlugin from "stylis-plugin-rtl";
+// بعد (جدید و درست):
+import rtlPlugin from "@mui/stylis-plugin-rtl";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Button } from "@mui/material";
+
+import { HelmetProvider, Helmet } from "react-helmet-async";
+
+// RTL theme
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "vazir, roboto",
+  },
+});
+
+// RTL cache
+const rtlCache = createCache({
+  key: "mui-rtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+function App() {
+  return (
+    <HelmetProvider>
+      <CacheProvider value={rtlCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Helmet>
+            <title>وب سایت شخصی محمد جواد ذاکریان</title>
+          </Helmet>
+          <div style={{ padding: 24 }}>
+            <h1>هدر</h1>
+            <p>محتوا</p>
+            <Button variant="contained">کلیک کن</Button>
+          </div>
+        </ThemeProvider>
+      </CacheProvider>
+    </HelmetProvider>
+  );
+}
+
+export default App;
