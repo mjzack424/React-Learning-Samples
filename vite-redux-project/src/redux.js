506eb7d92161e229fd0d59554a96c5d525227270
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-01-07 20:30:15 +0330

    191

---
+++ vite-redux-project/src/redux.js
@@ -0,0 +1,43 @@
+import { configureStore } from "@reduxjs/toolkit";
+//Actions
+const actionOne = { type: "counter/increment" };
+
+//Action Creators
+const increment = (number) => {
+  return {
+    type: "counter/increment",
+    payload: number,
+  };
+};
+
+//Reducers
+const reducerOne = (state, action) => {
+  return newState;
+};
+
+(state, action) => newState;
+
+const initialState = { value: 0 };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "counter/increment") {
+    const copyState = { ...state };
+    return {
+      value: (copyState.value += 1),
+    };
+  }
+
+  return state;
+};
+
+//Store
+const store = configureStore({ reducer: counterReducer });
+store.getState();
+
+//Dispatch
+store.dispatch({ type: "counter/increment" });
+
+or
+
+store.dispatch(increment());
+ 
