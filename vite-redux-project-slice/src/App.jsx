5bbc0924a15c294ae23beca62e42ca4a8c6e6e0b
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-01-08 08:39:50 +0330

    194

---
+++ vite-redux-project-slice/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import { store } from "../app/store"; 
+import { increment } from "../feadtures/counter/counterSlice";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => store.dispatch(increment())}>
+          count is {store.getState().value}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
