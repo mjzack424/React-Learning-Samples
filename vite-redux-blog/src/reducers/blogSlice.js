488fecd294ebc932cfff8e672b57cfd876ad060e
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-01-29 19:29:28 +0330

    200

---
+++ vite-redux-blog/src/reducers/blogSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+  {
+    id: nanoid(), //nanoid
+    date: new Date().toISOString(),
+    title: "پست نخست",
+    content: "محتوای جذاب اولین پست ما 😂",
+  },
+  {
+    id: nanoid(), //nanoid
+    date: new Date().toISOString(),
+    title: "پست دوم",
+    content: "عجب بالا و پایین داره دنیا!",
+  },
+];
+
+const blogsSlice = createSlice({
+  name: "blogs",
+  initialState: initialState,
+  reducers: {},
+});
+
+export default blogsSlice.reducer;
