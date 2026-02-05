c0846ee64e51ef35ca65c97457d5e712d086e0a4
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-05 19:28:43 +0330

    228
    RTK Query config

--- vite-redux-blog/src/store/index.js
+++ vite-redux-blog/src/store/index.js
@@ -2,12 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import blogsReducer from "../reducers/blogSlice";
 import usersReducer, { fetchUsers } from "../reducers/userSlice";
+import { apiSlice } from "../api/apiSlice"; //🆕
 
 export const store = configureStore({
   reducer: {
     blogs: blogsReducer,
     users: usersReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer, //🆕
   },
+  middleware: getDefaultMiddleware().concat(apiSlice.middleware), //🆕
+  //خط بالا میگه میدل ویر هایی که پیش فرض هست رو بگیر ادامش اضافه کن میدل ویر جدید رو
+  //این میدلویر برای طول عمر کش هست هر وقت ای پی آی الایس ساختیم باید این میدلویر رو هم بسازیم
 });
 
 //Fetch all users from API
