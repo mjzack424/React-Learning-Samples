d17fb037399a3b5f6695c4e3caa9aefe25b01061
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-12 19:25:15 +0330

    253

--- Sticker-Shop/src/main.jsx
+++ Sticker-Shop/src/main.jsx
@@ -4,14 +4,32 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./components/NotFound";
+import MainLayout from "./components/layouts/MainLayout";
+import { ToastContainer } from "react-toastify";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+  // {
+  //   path: "/products/:productID",
+  //   element:(<MainLayout><ProductDetails/></MainLayout>)
+  // },
+  // {
+  //   path:"/cart",
+  //   element:(<MainLayout><CartTable/></MainLayout>)
+  // }
+]);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>,
 );
