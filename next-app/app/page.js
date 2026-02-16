00b76cb15bd32f91d959b5962e385981a823a3b4
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-16 10:28:26 +0330

    269

--- next-app/app/page.js
+++ next-app/app/page.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>سلام چی شد؟!</h1>
+      <Link href={"/about"} prefetch>
+        درباره من
+      </Link>
+      <br />
+      <Link
+        href={{
+          pathname: "/about",
+          query: { name: "zack" },
+        }}
+      >
+        ممد برو
+      </Link>
+      <br />
+      <Link href={"/about"} replace>
+        درباره من-بدون برگشت!
+      </Link>
     </main>
   );
 }
