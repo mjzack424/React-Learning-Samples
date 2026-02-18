a8c5a31f672d40dcbd90c8a938a0d91aafdd45ed
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-18 10:49:26 +0330

    279

--- next-quiz-app/app/quiz/page.jsx
+++ next-quiz-app/app/quiz/page.jsx
@@ -51,6 +51,17 @@ const page = () => {
         ) : (
           <div className="quiz-container">
             <h3>نتایج</h3>
+            <h3>
+              به طور کلی {(result.score / 25) * 100}% سوالات جواب داده شده
+            </h3>
+            <p>کل سوالات :{questions.length}</p>
+            <p>کل امتیاز : {result.score}</p>
+            <p>سوالات درست : {result.correctAnswers}</p>
+            <p>سوالات غلط : {result.wrongAnswers}</p>
+
+            <button onClick={() => window.location.reload()}>
+              شروع مجدد آزمون
+            </button>
             {/* Show quiz result */}
           </div>
         )}
