3a2989b885d756937dbbfc48737102aa303011e2
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-18 17:37:56 +0330

    280

--- next-quiz-app/app/quiz/page.jsx
+++ next-quiz-app/app/quiz/page.jsx
@@ -17,12 +17,53 @@ const page = () => {
   });
 
   const { questions } = quiz;
-  const { answers, correctAnswers } = questions[activeQuestion];
+  const { answers, correctAnswer } = questions[activeQuestion];
+
+  // Select And Check'
+  const onAnswerSelected = (answer, index) => {
+    setChecked(true);
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+      console.log("True answer");
+    } else {
+      setSelectedAnswer(false);
+      console.log("False answer");
+    }
+  };
+
+  // Calculate score and increment to next question
+  const nextQuestion = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 },
+    );
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+    setChecked(false);
+  };
 
   return (
     <div className="container">
       <h1>صفحه آزمون</h1>
-      <div>{/* Quiz count*/}</div>
+      <div>
+        {!showResult ? (
+          <h2>
+            آزمون : {activeQuestion + 1} از
+            <span>{questions.length}</span>
+          </h2>
+        ) : null}
+      </div>
       <div>
         {!showResult ? (
           <div className="quiz-container">
@@ -30,7 +71,9 @@ const page = () => {
             {answers.map((answer, index) => (
               <li
                 key={index}
-                onClick={() => {}}
+                onClick={() => {
+                  onAnswerSelected(answer, index);
+                }}
                 className={
                   selectedAnswerIndex === index ? "li-selected" : "li-hover"
                 }
@@ -39,11 +82,11 @@ const page = () => {
               </li>
             ))}
             {checked ? (
-              <button className="btn" onClick={() => {}}>
+              <button className="btn" onClick={nextQuestion}>
                 {activeQuestion === questions.length - 1 ? "پایان" : "بعدی"}
               </button>
             ) : (
-              <button className="btn-disabled" onClick={() => {}} disabled>
+              <button className="btn-disabled" onClick={() => {nextQuestion}} disabled>
                 {activeQuestion === questions.length - 1 ? "پایان" : "بعدی"}
               </button>
             )}
