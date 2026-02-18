c5df72704d3537aa581de2ecaacbd16b40ac534d
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-18 09:21:23 +0330

    276

--- next-quiz-app/app/quiz/page.jsx
+++ next-quiz-app/app/quiz/page.jsx
@@ -1,5 +1,18 @@
 "use client";
+
+import { useState } from "react";
+
 const quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [checked, setChecked] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
   return (
     <>
       <h1>quiz</h1>
