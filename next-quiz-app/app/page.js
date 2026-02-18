2a64ade454021b4c215e19551770730415ba02eb
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-18 10:10:40 +0330

    277

--- next-quiz-app/app/page.js
+++ next-quiz-app/app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
 export default function home() {
-  <main>
-    <div className="container">
-      <h1>اپلیکیشن Quiz</h1>
-      <Link href={"/quiz"}>
-        <button>شروع آزمون</button>
-      </Link>
-    </div>
-  </main>;
+  return (
+    <main>
+      <div className="container">
+        <h1>اپلیکیشن Quiz</h1>
+        <Link href={"/quiz"}>
+          <button>شروع آزمون</button>
+        </Link>
+      </div>
+    </main>
+  );
 }
