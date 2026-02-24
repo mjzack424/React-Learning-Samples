d3e4f5e38603bb9de156371b9fd3e60e4ac85b85
mjzack424 <52859302+mjzack424@users.noreply.github.com>
2026-02-24 21:12:53 +0330

    311

--- pwa/src/App.js
+++ pwa/src/App.js
@@ -1,8 +1,74 @@
+import axios from "axios";
+import { useState } from "react";
+
 const App = () => {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchWeather = async () => {
+    const URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "14693e3980c297a8d34ac4a0a621f168";
+    if (!navigator.onLine) {
+      setError("خطای دسترسی به اینترنت❌!");
+      return;
+    }
+    setError(null);
+
+    try {
+      const { data } = await axios.get(URL, {
+        params: {
+          q: city,
+          units: "metric",
+          APPID: API_KEY,
+        },
+      });
+
+      console.log(data);
+      setWeather(data);
+      setCity("");
+    } catch (err) {
+      setError("شهری که دنبالشی رو پیدا نکردیم");
+      setWeather(null);
+      setCity("");
+    }
+  };
   return (
     <>
       <div className="main-container">
-        <input type="text" className="search" placeholder="نام شهر..." />
+        <input
+          type="text"
+          className="search"
+          placeholder="نام شهر..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? fetchWeather() : null)}
+        />
+        {error && (
+          <div className="city">
+            <h2 className="city-name">{error}</h2>
+          </div>
+        )}
+        {weather && (
+          <div className="city">
+            <h2 className="city-name">
+              <span>{weather.name}</span>
+              <sup>{weather.sys.country}</sup>
+            </h2>
+            <div className="city-temp">
+              {Math.round(weather.main.temp)}
+              <sup>&deg;C</sup>
+            </div>
+            <div className="info">
+              <img
+                className="city-icon"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt={weather.weather[0].description}
+              />
+              <p>{weather.weather[0].description}</p>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
